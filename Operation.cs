5c9f0f0127b93a4e0638163ef3eb59fb74ba08b6
Francisco Cano <volublecoffe@gmail.com>
2024-02-26 23:54:03 -0600

    commit

---
+++ Operation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_U3_Sobrecarga
+{
+    internal class Operation
+    {
+        // Method to add two numbers
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Method to add three numbers
+        public int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        // Method to subtract two numbers
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        // Method to subtract three numbers
+        public int Subtract(int a, int b, int c)
+        {
+            return a - b - c;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using POO_U3_Sobrecarga;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Operation operation = new Operation();
+
+        // Adding two numbers
+        Console.WriteLine("Sum of 5 and 3: " + operation.Sum(5, 3));
+
+        // Subtracting two numbers
+        Console.WriteLine("Difference of 5 and 3: " + operation.Subtract(5, 3));
+
+        // Adding three numbers
+        Console.WriteLine("Sum of 5, 3, and 2: " + operation.Sum(5, 3, 2));
+
+        // Subtracting three numbers
+        Console.WriteLine("Difference of 5, 3, and 2: " + operation.Subtract(5, 3, 2));
+    }
+}
